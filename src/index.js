f5dd639fb5694928f740659b81cec8fe54b173ed
aergashev <ae.ergashev@gmail.com>
2023-07-14 06:49:31 -0500

    form.js added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyDj-Ce5TjyU6oayttTw9AbA2SrtJoxrrtQ",
+  authDomain: "tinyturn-d1ce7.firebaseapp.com",
+  projectId: "tinyturn-d1ce7",
+  storageBucket: "tinyturn-d1ce7.appspot.com",
+  messagingSenderId: "58874125458",
+  appId: "1:58874125458:web:d176dee1a121bfca9eda4b",
+  measurementId: "G-667KPTGYC0",
+};
+
+initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
